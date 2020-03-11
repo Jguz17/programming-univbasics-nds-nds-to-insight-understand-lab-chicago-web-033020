c73451e4644b3c7468c30b67085cf820d54b3584
Johnathan  Guzman <johnathanguzman17@gmail.com>
2020-03-11 17:15:17 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,6 +5,10 @@ require 'pp'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds
+end
+
+def print_first_directors_movie_titles
   ss_movies = directors_database[0][:movies]
   index = 0
 
